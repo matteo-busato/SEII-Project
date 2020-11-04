29945b3e1b6b3e49ab2d70cc9a85af8ba8cbb3fc
matteo busato <matteo.busato-1@studenti.unitn.it>
2020-11-05 00:00:06 +0100

    index file node.js

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+
+// instantiate express
+const app = express();
+
+// set our port
+var port = process.env.PORT || 8080;
+
+// get an instance of the express Router
+var router = express.Router();
+
+// test route to make sure everything is working
+router.get('/test', function (req, res) {
+    res.json({ message: 'API is working correctly!' });
+});
+
+//####################################### SET ROUTER #################
+// register our router on /
+app.use('/', router);
+
+// handle invalid requests and internal error
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: { message: err.message } });
+});
+
+//####################################################################
+
+app.listen(port);
+console.log('EasyMusic on port ' + port);
