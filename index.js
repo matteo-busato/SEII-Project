a33580c28fe4244d5ad90c8080415111967c9410
Matteo Zanotto <matteo.zanotto@studenti.unitn.it>
2020-11-06 18:34:46 +0100

    updated functions with correct logic and URLs

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
 const { json } = require('body-parser');
 var express = require('express');
 
 // instantiate express
 const app = express();
-
 app.use(express.json());
-
-const insertMusic = require('./insertMusic/insertMusic.js');
 
 // set our port
 var port = process.env.PORT || 8080;
+
 
 // get an instance of the express Router
 var router = express.Router();
@@ -20,13 +17,18 @@ router.get('/test', function (req, res) {
     res.json({ message: 'API is working correctly!' });
 });
 
-//####################################### SET ROUTER #################
+//################## SET ROUTER #################
 // register our router on /
 app.use('/', router);
 
-app.post('/api/addNewAlbum', insertMusic.addNewAlbum);
-app.delete('/api/deleteAlbum', insertMusic.deleteAlbum);
-app.post('/api/changeAlbumData', insertMusic.changeAlbumData);
+
+//############# insertMusic part ################
+const insertMusic = require('./insertMusic/insertMusic.js');
+
+app.post('/api/v1/artists/:name/albums', insertMusic.addNewAlbum);
+app.delete('/api/v1/artists/:name/albums/:ismn', insertMusic.deleteAlbum);
+app.put('/api/v1/artists/:name/albums/:ismn', insertMusic.changeAlbumData);
+
 
 // handle invalid requests and internal error
 app.use((req, res, next) => {
@@ -39,6 +41,7 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({ error: { message: err.message } });
 });
+
 
 //####################################################################
 
--- insertMusic/insertMusic.js
+++ insertMusic/insertMusic.js
@@ -1,142 +1,193 @@
-const albumList = [
+// ########## MODULE FOR USER STORY #15 ############
+// lets artits add new albums, change data of existing albums, deleting albums
+// problems: - hardcoded variables and db data
+//           - incremental ID
+//           - maybe needs function to check if artist name and ismn are valid
+
+
+const artistList = [
     {
-        title: 'Album1',
-        artist: 'Artist1',
-        year: 1969,
-        genre: 'genre1'
+        name: "srv",
+        email: "srv@me.com",
+        bio: "cool blues guitarist"
     },
     {
-        title: 'Album2',
-        artist: 'Artist2',
-        year: 1971,
-        genre: 'genre1'
+        name: "black sabbath",
+        email: "black.sabbath@mail.com",
+        bio: "cool heavy metal band"
     },
     {
-        title: 'Album2',
-        artist: 'Artist1',
-        year: 1975,
-        genre: 'genre2'
+        name: "steely dan",
+        email: "no",
+        bio: "cool soft rock band"
     }
 ];
 
+const albumList = [
+    {
+        ismn: 1,
+        title: "metal album 1",
+        artist: "black sabbath",
+        year: 1970,
+        genre: "metal",
+        tracklist: ["track1", "track2", "track3"],
+        cost: 20
+    },
+    {
+        ismn: 2,
+        title: "metal album 2",
+        artist: "black sabbath",
+        year: 1972,
+        genre: "metal",
+        tracklist: ["track4", "track5", "track6"],
+        cost: 20
+    },
+    {
+        ismn: 3,
+        title: "blues album 1",
+        artist: "srv",
+        year: 1985,
+        genre: "blues",
+        tracklist: ["track1", "track2", "track3"],
+        cost: 20
+    },
+    {
+        ismn: 4,
+        title: "rock album 1",
+        artist: "steely dan",
+        year: 1975,
+        genre: "rock",
+        tracklist: ["track1", "track2", "track3"],
+        cost: 20
+    },
+    {
+        ismn: 5,
+        title: "rock album 2",
+        artist: "steely dan",
+        year: 1976,
+        genre: "rock",
+        tracklist: ["track1", "track2", "track3"],
+        cost: 20
+    }
+];
+
+var incrementalID = 6;
+
+// ######### DELETE ALBUM ##########
+// function for adding album with post method
 const addNewAlbum = (req, res) => {
+    let artist = req.params.name;
+
+    // verify if artist exists
+    let found = false;
+    artistList.forEach(element => {
+        if (element.name == artist) {
+            found = true;
+        }
+    });
+    if (!found) {
+        res.status(400).json({ error: 'this artist does not exist' });
+        return;
+    }
+
+    // create and check validity of new album data
     let album = {
+        ismn: incrementalID,
         title: req.body.title,
         artist: req.body.artist,
         year: parseInt(req.body.year),
-        genre: req.body.genre
+        tracklist: req.body.tracklist,
+        genre: req.body.genre,
+        cost: parseInt(req.body.cost)
     };
     let check = checkDataValidity(album);
     if (!check) {
         res.status(400).json({ error: 'error on the data of the given album' });
         return;
     } else {
-        let found = false;
-        albumList.forEach(element => {
-            if (element.artist == album.artist &&
-                element.title == album.title &&
-                element.year == album.year &&
-                element.genre == album.genre) {
-                found = true;
-            }
-        });
-        if (found) {
-            res.status(400).json({ error: 'this album has already been inserted in the db' });
-            return;
-        }
-
+        incrementalID = incrementalID + 1;
         albumList.push(album);
-        res.status(201).json({ message: 'the album has been correctly inserted in the db' });
+        res.status(201).json({ message: 'the album has been correctly inserted in the db with ISMN = '+album.ismn });
     }
 };
 
+// ######### DELETE ALBUM ##########
+// function for deleting album with delete method
 const deleteAlbum = (req, res) => {
-    let album = {
-        title: req.body.title,
-        artist: req.body.artist,
-        year: parseInt(req.body.year),
-        genre: req.body.genre
-    };
-    let check = checkDataValidity(album);
-    if (!check) {
-        res.status(400).json({ error: 'error on the data of the given album' });
-        return;
-    } else {
-        let found = false;
-        let index = 0;
-        for (index; index < albumList.length && !found; index++) {
-            var element = albumList[index];
-            if (element.artist == album.artist &&
-                element.title == album.title &&
-                element.year == album.year &&
-                element.genre == album.genre) {
-                found = true;
-                index = index - 1;
-            }
+    console.log('delete called');
+    let artist = req.params.name;
+    let ismn = parseInt(req.params.ismn);
+    
+    // verify if (artist name, ismn) exists in db
+    let found = false;
+    let index = 0;
+    for (index; index < albumList.length && !found; index++) {
+        var element = albumList[index];
+        if (element.ismn == ismn && element.artist == artist) {
+            found = true;
+            index = index - 1;
         }
-        if (!found) {
-            res.status(400).json({ error: 'this album is not present in the db' });
-            return;
-        }
-        albumList.splice(index, 1);
-        res.status(201).json({ message: 'the album has been correctly deleted from the db' });
     }
+    if (!found) {
+        res.status(400).json({ error: 'this album does not exist in the db' });
+        return;
+    }
+    albumList.splice(index, 1);
+    res.status(201).json({ message: 'the album has been correctly deleted from the db' });
 };
 
+
+// ######### CHANGE ALBUM DATA ##########
+// function for changing album data with put method
 const changeAlbumData = (req, res) => {
-    let oldData = {
-        title: req.body.oldTitle,
-        artist: req.body.oldArtist,
-        year: parseInt(req.body.oldYear),
-        genre: req.body.oldGenre
+    console.log('put called');
+    let artist = req.params.name;
+    let ismn = parseInt(req.params.ismn);
+
+    // verify if (artist name, ismn) exists in db
+    let found = false;
+    let index = 0;
+    for (index; index < albumList.length && !found; index++) {
+        var element = albumList[index];
+        if (element.ismn == ismn && element.artist == artist) {
+            found = true;
+            index = index - 1;
+        }
+    }
+    if (!found) {
+        res.status(400).json({ error: 'this album does not exist in the db' });
+        return;
+    }
+
+    // create and check validity of updated album data
+    let newData = {
+        title: req.body.title,
+        year: parseInt(req.body.year),
+        tracklist: req.body.tracklist,
+        genre: req.body.genre,
+        cost: parseInt(req.body.cost)
     };
-    let check = checkDataValidity(oldData);
+    let check = checkDataValidity(newData);
     if (!check) {
-        res.status(400).json({ error: 'error on the old data of the given album' });
+        res.status(400).json({ error: 'error on the new data of the given album' });
         return;
     } else {
-        let newData = {
-            title: req.body.newTitle,
-            artist: req.body.newArtist,
-            year: req.body.newYear,
-            genre: req.body.newGenre
-        };
-        let check = checkDataValidity(newData);
-        if (!check) {
-            res.status(400).json({ error: 'error on the new data of the given album' });
-            return;
-        } else {
-            let found = false;
-            let index = 0;
-            for (index; index < albumList.length && !found; index++) {
-                var element = albumList[index];
-                if (element.artist == oldData.artist &&
-                    element.title == oldData.title &&
-                    element.year == oldData.year &&
-                    element.genre == oldData.genre) {
-                    found = true;
-                    index = index - 1;
-                }
-            }
-            if (!found) {
-                res.status(400).json({ error: 'this album is not present in the db' });
-                return;
-            }
-            albumList[index].title = newData.title;
-            albumList[index].artist = newData.artist;
-            albumList[index].year = newData.year;
-            albumList[index].genre = newData.genre;
-            res.status(201).json({ message: 'the album data has been correctly changed in the db' });
-        }
+
+        // update album data in the db
+        albumList[index].title = newData.title;
+        albumList[index].year = newData.year;
+        albumList[index].tracklist = newData.tracklist;
+        albumList[index].genre = newData.genre;
+        albumList[index].cost = newData.cost;
+        res.status(201).json({ message: 'the album data has been correctly changed in the db' });
     }
 };
 
+
+// ######### CHECK ALBUM DATA VALIDITY ##########
+// function for checking validity of album data in input
 const checkDataValidity = (album) => {
     if (!album.title || typeof album.title != 'string') {
-        return false;
-    }
-
-    if (!album.artist || typeof album.artist != 'string') {
         return false;
     }
 
@@ -147,6 +198,21 @@ const checkDataValidity = (album) => {
     if (isNaN(album.year)) {
         return false;
     }
+
+    if (!album.tracklist) {
+        return false;
+    } else {
+        album.tracklist.forEach(track => {
+            if (!track || typeof track != 'string') {
+                return false;
+            }
+        });
+    }
+
+    if (isNaN(album.cost)) {
+        return false;
+    }
+
     return true;
 }
 
