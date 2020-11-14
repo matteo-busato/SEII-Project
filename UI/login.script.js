7c86f5b4d8d19fd6dfed00874058d44376065658
matteo busato <matteo.busato-1@studenti.unitn.it>
2020-11-14 14:40:03 +0100

    trying to implement something with database

---
+++ UI/login.script.js
@@ -0,0 +1,27 @@
+
+
+var submit = function(){
+    var email = document.getElementById("email").innerText;
+    var password = document.getElementById("password").innerText;
+
+    console.log(email);
+    console.log(password);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.responseType = "json";
+
+    xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var res = this.response;
+        console.log(res);
+
+
+    }
+}
+    xhttp.open("post", "localhost:8080/api/v1/insert", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.setRequestHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    xhttp.send("email=" + email + "&password=" + password );
+
+}
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
+var bodyparser = require('body-parser');
 var express = require('express');
 
 // instantiate express
 const app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 // set our port
 var port = process.env.PORT || 8080;
@@ -10,25 +12,75 @@ var port = process.env.PORT || 8080;
 // get an instance of the express Router
 var router = express.Router();
 
-// test route to make sure everything is working
-router.get('/test', function (req, res) {
-    res.json({ message: 'API is working correctly!' });
+app.use(express.static('UI'));
+app.use('/', router);
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
-var artists = [{name:"gianni",email:"gitd@com.it",bio:"cioa"},
-                {name:"piero pelù",email:"assodicoppe@com.it",bio:"miciomiao"}];
-
-router.route('/artists')
-        .get(function (req, res) {
-                res.json(artists);
+//connection to database
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected to db");
 });
 
-router.route('/artists/:artist-name')
-    .get(function(req,res){
-        var name = req.params.name;
-        console.log(artists[name]);
-            res.json(artists[name]);
+const User = require('./models/user.js');
+// instantiate express
+
+
+// insert a predefined user
+app.post('/api/v1/insert'  ,function (req, res) {
+
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
     });
+
+    console.log(user.email);
+    console.log(user.password);
+
+    user.save( function (err,data) {
+        if(err){
+            res.status(400).json({message: "error saving user." + err});
+        }
+        else{
+            res.status(200).json({message: "saved new user." + user});
+        }
+    });
+});
+
+//get all the users
+router.get('/find', function (req, res) {
+    User.find( function (err,utente) {
+        if(err) res.json({message: "error getting users."});
+        else{
+            var message = "users: ";
+            res.json({message: utente });
+        }
+    });
+});
+
+router.get('/drop', function(req,res){
+
+    db.collection("user").remove(function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("Collection deleted");
+  });
+})
+
+app.get('/login', (req, res) => {
+
+        res.sendFile('UI/login.html', {root:'./'}, (err) => {
+        res.end();
+        if(err) throw(err);
+    });
+
+});
 
 //####################################### SET ROUTER #################
 // register our router on /
