ea91cc1845d6630a9aa6c1b49558a395c53e631f
Dagu9 <davide.guidolin@studenti.unitn.it>
2020-11-22 18:17:44 +0100

    Added some tests

--- test/userStory4.test.js
+++ test/userStory4.test.js
@@ -3,24 +3,46 @@ const api = require('../api/userStory4');
 const request = require('supertest');
 const mongoose = require('mongoose');
 
-describe('GET /api/v1/artists', () => {
+describe('GET /api/v1/artists/', () => {
 
     afterAll(async () => {
         try {
           // Connection to Mongo killed.
           await mongoose.disconnect();
         } catch (error) {
-          console.log(`
-            You did something wrong dummy!
-            ${error}
-          `);
+          console.log(`Error: ${error} `);
           throw error;
         }
     });
     
-    it('GET /api/v1/artists ', function (done) {
-        request(index)
-        .get('/api/v1/artists')
-        .expect(200, done);
+    it('GET /api/v1/artists/:name with a name not present in db should return an error', function (done) {
+      request(index)
+      .get('/api/v1/artists/NotInDB')
+      .expect(404, done);
     });
+
+    it('GET /api/v1/artists/:name/albums/:ismn with a name not present in db should return an error', function(done){
+      request(index)
+      .get('/api/v1/artists/NotInDB/albums/2')
+      .expect(404, done);
+    });
+
+    it('GET /api/v1/artists/:name/albums/:ismn with a non number ismn should return an error', function(done){
+      request(index)
+      .get('/api/v1/artists/Meshuggah/albums/AAAA')
+      .expect(400, { error: 'error on ismn param'}, done);
+    });
+
+    it('GET /api/v1/artists/:name/albums/:ismn with a negative ismn should return an error', function(done){
+      request(index)
+      .get('/api/v1/artists/Meshuggah/albums/-10')
+      .expect(404, { error: 'The album -10 does not exist' }, done);
+    });
+    
+    it('GET /api/v1/artists/:name/albums/:ismn with a float ismn should use the integer part of the ismn', function(done){
+      request(index)
+      .get('/api/v1/artists/Meshuggah/albums/10.12')
+      .expect(404,{ error: 'The album 10 does not exist' }, done);
+    });
+
 });
