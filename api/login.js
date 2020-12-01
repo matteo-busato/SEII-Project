9d225da9973d69166893b0db9e66209bf79ea8ef
Dagu9 <davide.guidolin@studenti.unitn.it>
2020-12-01 11:33:39 +0100

    login hotfix

--- api/login.js
+++ api/login.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const User = require('../models/user.js');
 
-function checkEmail(email){
+function checkEmail(email) {
     re = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
 
-    if(re.test(email))
+    if (re.test(email))
         return true;
     else
         return false;
@@ -26,51 +26,55 @@ const auth = (req, res) => {
 
 
     //check for problems
-    if(!email || typeof email != 'string' ){
+    if (!email || typeof email != 'string') {
         res.status(404).json("The field 'email' must be a non-empty string");
         return;
     }
-    if(!checkEmail(email)){
-        res.status(404).json({ error: "Invalid email format"});
+    if (!checkEmail(email)) {
+        res.status(404).json({ error: "Invalid email format" });
         return;
     }
-    if(!password || typeof password != 'string'){
-        res.status(401).json({ error: "The field 'password' must be a non-empty string"});
+    if (!password || typeof password != 'string') {
+        res.status(401).json({ error: "The field 'password' must be a non-empty string" });
         return;
     }
 
-    User.findOne( { email : email }, function (err,user) {
-        if(!err && user){   //we look at password matching
+    User.findOne({ email: email }, function (err, user) {
+        if (!err && user) {   //we look at password matching
 
-            bcrypt.compare( password, user.password, function (err, match) {
-                if(match) { //matched password
+            bcrypt.compare(password, user.password, function (err, match) {
+                if (err) {
+                    status = 500;
+                    result.status = status;
+                    result.error = err;
+                    res.status(status).send(result);
+                    return;
+                }
+                if (match) { //matched password
                     //implements jsonwebtoken
-                    const payload = { username: user.username,      //to know if has right to change albums/events/merch
-                                        userType: user.userType,
-                                        id: user._id                }; //to know what type of user is
+                    const payload = {
+                        username: user.username,      //to know if has right to change albums/events/merch
+                        userType: user.userType,
+                        id: user._id
+                    }; //to know what type of user is
                     const options = { expiresIn: '1d', issuer: 'http://localhost:8080/' };
                     const token = jwt.sign(payload, process.env.SECRET, options);
                     result.token = token;
                     result.status = status;
                     result.username = user.username;
                     result.userType = user.userType;
-                } else if(!match){
+                    res.status(status).json(result);
+                    return;
+                } else {
                     status = 401;
                     result.status = status;
                     result.error = 'wrong password';
                     res.status(status).json(result);
                     return;
                 }
-                if (err) {
-                status = 500;
-                result.status = status;
-                result.error = err;
-                res.status(status).send(result);
-                return;
-            }
-        });
+            });
 
-        }else{      //email not matching
+        } else {      //email not matching
             status = 404;
             result.status = status;
             result.error = "wrong email";
