7f648761c3406874932500670b69522da80dd445
Dagu9 <davide.guidolin@studenti.unitn.it>
2020-12-03 09:54:34 +0100

    Added comments in events.js

--- api/events.js
+++ api/events.js
@@ -1,32 +1,21 @@
-const artists = [{ username: "autechre", email: "aemusic@ae.com", bio: "electronic duo", userType:'artist' },
-{ username: "bob marley", email: "bob.marley@me.com", bio: "weed", userType:'artist' },
-{ username: "beatles", email: "betles@me", bio: "softrockband", userType:'artist' },
-{ username: "Meshuggah", email: "mesh@me", bio: "catsNcats", userType:'artist' },
-{ username: "MeshuggahFanBoy", email: "meshFan@me", bio: "catsNcats", userType:'user' }];
-
-  
 const bcrypt = require('bcrypt');
 const Event = require('../models/event');
 const User = require('../models/user');
 
-//INSERT ARTISTS FOR TESTING
 /*
-for(a of artists){
-    a.password = a.username+'1234';
-    User(a).save();
-}
+*   check if the given date match a regular expression dd/mm/yyyy hh:mm
 */
-
 function checkDate(date) {
-    let re = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}[\ ](0?[0-9]|[1][0-9]|[2][0-3]):(0?[0-9]|[12345][0-9])$/);
+    let re = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}[\ ]([0][0-9]|[1][0-9]|[2][0-3]):([0][0-9]|[12345][0-9])$/);
 
-    if (re.test(date))
-        return true;
-    else
-        return false;
+    return re.test(date);
 }
 
+/*
+*   convert a date string in the form dd/mm/yyyy hh:mm to a Date object
+*/
 function toDate(dateStr) {
+    // split in days+months+years and hours+minutes
     let dmy = dateStr.split(" ")[0];
     let hm = dateStr.split(" ")[1];
 
@@ -38,37 +27,42 @@ function toDate(dateStr) {
 }
 
 /*
-    Add new events
-    POST api/v1/artists/:name/events
+*   Add new events
+*   POST api/v1/artists/:name/events
 */
 const addEvent = async (req, res) => {
 
-    if(!req.loggedUser){
-        res.status(401).json({error: "Please authenticate first"});
+    // check if the user is logged
+    if (!req.loggedUser) {
+        res.status(401).json({ error: "Please authenticate first" });
         return;
     }
 
+    // get artist parameter
     let artist = req.params.name;
 
-    if(req.loggedUser.userType != 'artist'){
-        res.status(401).json({error: "You must be an artist to access this page"});
+    // check if the user is an artist
+    if (req.loggedUser.userType != 'artist') {
+        res.status(401).json({ error: "You must be an artist to access this page" });
         return;
     }
 
-    let artistIn = await User.findOne({'username':artist, 'userType':'artist'}, (err) => {
-        if(err) {
+    // check if the artist exists in the db
+    let artistIn = await User.findOne({ 'username': artist, 'userType': 'artist' }, (err) => {
+        if (err) {
             res.status(500).json({ error: err });
             return;
         }
     });
 
-    if(!artistIn){
+    if (!artistIn) {
         res.status(404).json({ error: 'The artist ' + artist + ' does not exist' });
         return;
     }
 
-    if(req.loggedUser.username != artist){
-        res.status(401).json({error: "You can't add an event for this artist"});
+    // check if the artist is trying to add another artist's event
+    if (req.loggedUser.username != artist) {
+        res.status(401).json({ error: "You can't add an event for this artist" });
         return;
     }
 
@@ -82,12 +76,13 @@ const addEvent = async (req, res) => {
         owner: artist
     }
 
-    //checks
+    // checks for data types and formats
     if (isNaN(event.id) || event.id < 0) {
         res.status(400).json({ error: "The field 'id' must be an integer greater than 0" });
         return;
     }
 
+    // check if there is already an event with that id
     let isIn = await Event.findOne({ 'id': event.id, 'owner': artist }, function (err) {
         if (err)
             if (err.kind == "ObjectId")
@@ -119,11 +114,13 @@ const addEvent = async (req, res) => {
     }
     event.date = toDate(event.date);
 
-    if (event.date < Date.now()){
+    // check if the date is a past date
+    if (event.date < Date.now()) {
         res.status(400).json({ error: "You can't add an event for a date in the past" });
         return;
     }
 
+    // check if there is already an event in that date
     let dates = await Event.findOne({ 'date': event.date, 'owner': artist }, function (err) {
         if (err) {
             res.status(500).json({ error: err });
@@ -152,6 +149,7 @@ const addEvent = async (req, res) => {
     }
 
 
+    // Create and insert the new event
     let newEvent = new Event(event);
     newEvent = await newEvent.save(function (err, result) {
         if (err)
@@ -162,47 +160,54 @@ const addEvent = async (req, res) => {
 }
 
 /*
-    Remove an event
-    DELETE /api/v1/artists/:name/events/:id
+*   Remove an event
+*   DELETE /api/v1/artists/:name/events/:id
 */
 const removeEvent = async (req, res) => {
 
-    if(!req.loggedUser){
-        res.status(401).json({error: "Please authenticate first"});
+    // check if the user is logged
+    if (!req.loggedUser) {
+        res.status(401).json({ error: "Please authenticate first" });
         return;
     }
 
+    // get artist parameter
     let artist = req.params.name;
-    
-    if(req.loggedUser.userType != 'artist'){
-        res.status(401).json({error: "You must be an artist to access this page"});
+
+    // check if the user is an artist
+    if (req.loggedUser.userType != 'artist') {
+        res.status(401).json({ error: "You must be an artist to access this page" });
         return;
     }
 
-    let artistIn = await User.findOne({'username':artist, 'userType':'artist'}, (err) => {
-        if(err) {
+    // check if the artist exists in the db
+    let artistIn = await User.findOne({ 'username': artist, 'userType': 'artist' }, (err) => {
+        if (err) {
             res.status(500).json({ error: err });
             return;
         }
     });
-    
-    if(!artistIn){
+
+    if (!artistIn) {
         res.status(404).json({ error: 'The artist ' + artist + ' does not exist' });
         return;
     }
 
-    if(req.loggedUser.username != artist){
-        res.status(401).json({error: "You can't delete an event for this artist"});
+    // check if the artist is trying to delete another artist's event
+    if (req.loggedUser.username != artist) {
+        res.status(401).json({ error: "You can't delete an event for this artist" });
         return;
     }
 
     let id = parseInt(req.params.id);
 
+    // checks for data types and formats
     if (isNaN(id) || id < 0) {
         res.status(400).json({ error: "The field 'id' must be an integer greater than 0" });
         return;
     }
 
+    // check if there is an event with that id
     let isIn = await Event.findOne({ 'id': id, 'owner': artist }, function (err) {
         if (err)
             if (err.kind == "ObjectId")
@@ -218,6 +223,7 @@ const removeEvent = async (req, res) => {
         return;
     }
 
+    // delete the event
     await Event.remove({ 'id': id, 'owner': artist }, function (err) {
         if (err)
             res.status(500).json({ error: err });
@@ -228,47 +234,54 @@ const removeEvent = async (req, res) => {
 }
 
 /*
-    Change event data
-    PUT /api/v1/artists/:name/events/:id
+*   Change event data
+*   PUT /api/v1/artists/:name/events/:id
 */
 const changeEvent = async (req, res) => {
 
-    if(!req.loggedUser){
-        res.status(401).json({error: "Please authenticate first"});
+    // check if the user is logged
+    if (!req.loggedUser) {
+        res.status(401).json({ error: "Please authenticate first" });
         return;
     }
 
+    // get artist parameter
     let artist = req.params.name;
 
-    if(req.loggedUser.userType != 'artist'){
-        res.status(401).json({error: "You must be an artist to access this page"});
+    // check if the user is an artist
+    if (req.loggedUser.userType != 'artist') {
+        res.status(401).json({ error: "You must be an artist to access this page" });
         return;
     }
 
-    let artistIn = await User.findOne({'username':artist, 'userType':'artist'}, (err) => {
-        if(err) {
+    // check if the artist exists in the db
+    let artistIn = await User.findOne({ 'username': artist, 'userType': 'artist' }, (err) => {
+        if (err) {
             res.status(500).json({ error: err });
             return;
         }
     });
 
-    if(!artistIn){
+    if (!artistIn) {
         res.status(404).json({ error: 'The artist ' + artist + ' does not exist' });
         return;
     }
 
-    if(req.loggedUser.username != artist){
-        res.status(401).json({error: "You can't modify an event for this artist"});
+    // check if the artist is trying to modify another artist's event
+    if (req.loggedUser.username != artist) {
+        res.status(401).json({ error: "You can't modify an event for this artist" });
         return;
     }
 
     let id = parseInt(req.params.id);
 
+    // checks for data types and formats
     if (isNaN(id) || id < 0) {
         res.status(400).json({ error: 'The id field should be an integer greater than 0' });
         return;
     }
 
+    // check if there is an event with that id
     let isIn = await Event.findOne({ 'id': id, 'owner': artist }, function (err) {
         if (err)
             if (err.kind == "ObjectId")
@@ -293,9 +306,7 @@ const changeEvent = async (req, res) => {
         cost: req.body.cost
     }
 
-    //console.log("NEW EVENT = ", newEvent);
-
-    //Errors checks
+    // Various data types and formats checks
     if (newEvent.title != undefined && (!newEvent.title || typeof newEvent.title != 'string')) {
         res.status(400).json({ error: "The field 'title' must be a non-empty string" });
         return;
@@ -314,11 +325,13 @@ const changeEvent = async (req, res) => {
     if (newEvent.date != undefined) {
         newEvent.date = toDate(newEvent.date);
 
-        if (newEvent.date < Date.now()){
+        // check if the date is a past date
+        if (newEvent.date < Date.now()) {
             res.status(400).json({ error: "The new date can't be a date in the past" });
             return;
         }
 
+        // check if there is already an event in that date
         let dates = await Event.findOne({ 'date': newEvent.date, 'owner': artist }, function (err) {
             if (err) {
                 res.status(500).json({ error: err });
@@ -349,7 +362,7 @@ const changeEvent = async (req, res) => {
         }
     }
 
-    //Data insertion
+    // Insert data in the db
     if (newEvent.title) {
         await Event.updateOne({ 'id': id, 'owner': artist }, { $set: { 'title': newEvent.title } }, (err) => {
             if (err) {
