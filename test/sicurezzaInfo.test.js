4cf2d4922b32f8acb79625c974e4035fdea992ef
Riccardo Persico <riccardo.persico@studenti.unitn.it>
2020-12-14 13:33:03 +0100

    Fixed logout for info section of an artist

--- test/sicurezzaInfo.test.js
+++ test/sicurezzaInfo.test.js
@@ -35,18 +35,20 @@ describe("test security on apis", () => {
         .expect(401, { error: 'Please login first' }, done)
     });
 
-    it('POST /api/v1/artists/:name/manageInfo with the token of another artist should return an error', async function(done) {
+    test('post info with valid token to another artist', async function (done) {
         request(index)
-        .post('/api/v1/artists/ale/manageInfo')
+        .post('/api/v1/artists/bob/manageInfo')
+        .set('Content-Type', 'application/json')
         .send({
-            token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJvYiIsInVzZXJUeXBlIjoiYXJ0aXN0IiwiaWQiOiI1ZmI2OWJhMjlhZmU1YjI1ODA4N2Q4ZDAiLCJpYXQiOjE2MDcxNzIwNDIsImV4cCI6MTYwNzI1ODQ0MiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwLyJ9.feCxr4nNL409go_4QQ1YYTNnGxW1xw-GsfGx1GftAn8",
-            email: "ale@gmail.com",
-            username: "Ale",
-            password: "ale1234",
+            // needs new token every time
+            token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsZSIsInVzZXJUeXBlIjoiYXJ0aXN0IiwiaWQiOiI1ZmI2OWI5NjlhZmU1YjI1ODA4N2Q4Y2YiLCJpYXQiOjE2MDc5NDg3ODEsImV4cCI6MTYwODAzNTE4MSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwLyJ9.VVkzW6ZxnMDY2gyNCgL7VbTUWMae3IZ7-C1-bRRu9Z0",
+            email: "bob@gmail.com",
+            username: "bob",
+            password: "$2b$10$e92CJEpRB1hmZlCOTa7OO.v2KtPxw13wCWjxTlOBLV6HO5KUldwa.",
             userType: "artist",
             bio: "Cool Rock Artist"
         })
-        .expect(401, {error: "You can't add an product for this artist"}, done)
+        .expect(401, { error: 'You can\'t add info for this artist' }, done);
     });
 
     it('DELETE /api/v1/artists/:name/manageInfo with an invalid token should return an error', async function(done) {
@@ -66,3 +68,5 @@ describe("test security on apis", () => {
         .expect(401, { error: 'Please login first' }, done)
     });
 });
+
+
